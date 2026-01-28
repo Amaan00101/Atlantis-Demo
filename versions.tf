f27da76a4ef3ac778180efda3f9db967acc49b6b
Amaan Mansuri <165874699+Amaan00101@users.noreply.github.com>
2026-01-28 16:17:10 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.0.0"
 
+  backend "s3" {
+    bucket  = "atlantis-demo-tf-stateee-file"
+    key     = "atlantis/demo/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+
   required_providers {
     null = {
       source  = "hashicorp/null"
